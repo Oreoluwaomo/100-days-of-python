bb6e83de2469a108d87179f234de01767dd4e96c
Oreoluwaomo <oreoluwaomojuwa@gmail.com>
2025-06-01 14:46:50 +0100

    Initial commit

---
+++ Day 1/task/task.py
@@ -0,0 +1,7 @@
+print('Welcome to the Band Name Generator ! ')
+city = input("What is the name of the city you grew up in ? ")
+pet = input("what is the name of "
+            "the pet you had or you ? "
+            "have now")
+band_name = city + pet
+print(band_name)
