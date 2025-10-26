fb9fd4e414772898f454f6a8b409e924bbff772b
zeal <oreoluwaomojuwa@gmail.com>
2025-10-26 16:24:26 +0100

    Add Menu and MenuItem classes for drink management

---
+++ Day-16-start/menu.py
@@ -0,0 +1,32 @@
+class MenuItem:
+    """Models each Menu Item."""
+    def __init__(self, name, water, milk, coffee, cost):
+        self.name = name
+        self.cost = cost
+        self.ingredients = {
+            "water": water,
+            "milk": milk,
+            "coffee": coffee
+        }
+
+
+class Menu:
+    """Models the Menu with drinks."""
+    def __init__(self):
+        self.menu = [
+            MenuItem(name="latte", water=200, milk=150, coffee=24, cost=2.5),
+            MenuItem(name="espresso", water=50, milk=0, coffee=18, cost=1.5),
+            MenuItem(name="cappuccino", water=250, milk=50, coffee=24, cost=3.0),
+        ]
+
+    def get_items(self):
+        """Returns all the names of available drinks."""
+        return "/".join([item.name for item in self.menu])
+
+    def find_drink(self, order_name):
+        """Searches for a drink by name."""
+        for item in self.menu:
+            if item.name == order_name:
+                return item
+        print("Sorry, that item is not available.")
+        return None
